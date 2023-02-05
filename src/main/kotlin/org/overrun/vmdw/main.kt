14537ed42bd3620711f833efb15799c5473119fb
baka4n <qq474899581@gmail.com>
2023-02-05 23:56:14 +0800

    add center button test

--- src/main/kotlin/org/overrun/vmdw/main.kt
+++ src/main/kotlin/org/overrun/vmdw/main.kt
@@ -25,27 +25,21 @@
 package org.overrun.vmdw
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import androidx.compose.ui.window.singleWindowApplication
+import java.awt.Label
 
 
 @Composable
@@ -54,11 +48,17 @@ fun App() {
     var text by remember { mutableStateOf("test ui") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "test button"
-        }) {
-            Text(text)
+        Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {
+                    text = "test button"
+                }
+            ) {
+                Text(text)
+            }
         }
+
     }
 
 }
