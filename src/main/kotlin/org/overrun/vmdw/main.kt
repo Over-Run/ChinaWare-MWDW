0ef32621d84eb73f94a0ebf1d59c5f1308163d71
baka4n <qq474899581@gmail.com>
2023-02-09 00:15:20 +0800

    center window and create null settings

--- src/main/kotlin/org/overrun/vmdw/main.kt
+++ src/main/kotlin/org/overrun/vmdw/main.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyShortcut
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.*
 import org.overrun.vmdw.config.Config
 import java.awt.Desktop
@@ -70,7 +70,7 @@ fun windowContent(scope: FrameWindowScope) = scope.run {
  * @author squid233, baka4n
  * @since 0.1.0
  */
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterialApi::class, ExperimentalUnitApi::class)
 fun main() {
     // Note: Do NOT use expression body. That cause the initialization reruns.
     Config.init()
@@ -78,13 +78,20 @@ fun main() {
 
     application {
         var isOpen by remember { mutableStateOf(true) }
-        var isAboutOpen by remember { mutableStateOf(true) }
-
+        var isAboutOpen by remember { mutableStateOf(false) }
+        var isSettingOpen by remember { mutableStateOf(false) }
+        var width by remember { mutableStateOf(1000) }
+        var height by remember { mutableStateOf(800) }
+        val datas = mutableListOf("english[us]", "简体中文")
         if (isOpen) {
+
             Window(
                 onCloseRequest = { isOpen = false },
                 title = "ChinaWare VMDW",
-                state = rememberWindowState(width = 800.dp, height = 640.dp),
+                state = WindowState(
+                    size = DpSize(width.dp, height.dp),
+                    position = WindowPosition(Alignment.Center)
+                ),
                 icon = painterResource("icon.png")
             ) {
                 MenuBar {
@@ -92,8 +99,10 @@ fun main() {
                         Item(
                             I18n["menu.file.settings"],
                             mnemonic = 'T',
-                            shortcut = KeyShortcut(Key.S, ctrl = true, alt = true)
-                        ) { }
+                            shortcut = KeyShortcut(Key.S, ctrl = true, alt = true),
+                        ) {
+                            isSettingOpen = true
+                        }
                         Item(I18n["menu.file.exit"], mnemonic = 'X') { isOpen = false }
                     }
                     Menu(I18n["menu.edit"], mnemonic = 'E') { }
@@ -109,7 +118,11 @@ fun main() {
             Dialog(
                 onCloseRequest = { isAboutOpen = false },
                 title = I18n["dialog.about.title"],
-                state = DialogState(width = 300.dp, height = 300.dp)
+                state = DialogState(
+                    width = 300.dp,
+                    height = 300.dp,
+                    position = WindowPosition(Alignment.Center)
+                )
             ) {
                 // 关于内容
                 Column {
@@ -126,10 +139,27 @@ fun main() {
                         modifier = Modifier.align(Alignment.CenterHorizontally),
                         shape = RoundedCornerShape(1.dp)
                     ) {
-                        Text(text = "ChinaWare VMDW", color = Color.Blue)
+                        Text(
+                            text = "ChinaWare VMDW",
+                            color = Color.Blue,
+                            fontSize = TextUnit(12f, TextUnitType.Sp))
                     }
                     Text(text = "  ChinaWare VMDW full name is ChinaWare Visualize Minecraft Development Wheel")
                 }
+            }
+        }
+
+        if (isSettingOpen) {
+            Dialog(
+                onCloseRequest = {isSettingOpen = false},
+                title = I18n["dialog.settings.title"],
+                state = DialogState(
+                    width = 800.dp,
+                    height = 640.dp,
+                    position = WindowPosition(Alignment.Center)
+                )
+            ) {
+                
             }
         }
     }
